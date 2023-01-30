b08458ad54b95d98da08515e877ed2e2330c4a77
Andrimarciely Souza <andrimarciely@gmail.com>
2023-01-30 16:14:49 -0400

    configuração do admin de Dados

--- dados/admin.py
+++ dados/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Parametro, Amostra
 # Register your models here.
+admin.site.register(Parametro)
+admin.site.register(Amostra)
--- dados/models.py
+++ dados/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Parametro(models.Model):
+    descricao_parametro=models.CharField(max_length=100, help_text='nome do agente químico')
+    unidade_de_medida_do_parametro=models.CharField(max_length=10, help_text='unidade de medida do agente químico')
+    resultado=models.CharField(max_length=10, help_text='valor mensurado do agente químico na amostra')
+    lq=models.CharField(max_length=100, help_text='valor de referência')
+
+    def __str__(self):
+        return self.descricao_parametro
+
+
+class Amostra(models.Model):
+    codigo_interno=models.CharField(max_length=50, help_text='Código de identificação interna ACME')
+    setor_local_avaliado=models.CharField(max_length=100, help_text='')
+    hora_da_coleta=models.CharField(max_length=100, help_text='')
+    data_da_coleta=models.DateField()
+    metodo_de_analise=models.CharField(max_length=100, help_text='')
+    descricao_do_metodo=models.CharField(max_length=100, help_text='')
+    data_de_entrada_no_laboratorio=models.DateField()
+    data_do_relatorio_de_ensaio=models.DateField()
+    unidade_de_medida=models.CharField(max_length=100, help_text='')
+    volume=models.CharField(max_length=10, help_text='')
+    conclusao=models.CharField(max_length=200, help_text='')
+    observacao=models.CharField(max_length=200, help_text='')
+    irregularidades=models.CharField(max_length=200, help_text='')
+    parametro = models.ForeignKey(Parametro, on_delete=models.CASCADE)
+
+
+
